1ec3ed91cb75b245520c25c8e337a49251fe8abf
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-01-29 13:25:35 +0100

    feat(backend): initial spring boot setup with mongodb and env vars

---
+++ src/main/java/com/traintogain/backend/TtgAppApplication.java
@@ -0,0 +1,13 @@
+package com.traintogain.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtgAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtgAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/traintogain/backend/TtgAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traintogain.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtgAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
