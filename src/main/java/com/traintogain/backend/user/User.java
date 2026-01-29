74cd71198affeecdafcbe57bf0499313b5a99f9b
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-01-29 14:04:01 +0100

    feat(user): add User entity

---
+++ src/main/java/com/traintogain/backend/user/User.java
@@ -0,0 +1,60 @@
+package com.traintogain.backend.user;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String email;
+    private String username;
+    private String password;
+
+    // später: Einstellungen, Rollen, etc.
+
+    public User() {
+    }
+
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter & Setter
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
