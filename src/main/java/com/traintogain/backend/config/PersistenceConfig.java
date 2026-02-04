dd90e24cc71f10a0ce7db38a9da99704d8cc3ec2
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-02-04 17:30:14 +0100

    fix(persistence): include passwordreset mongo repository in repository scan

--- src/main/java/com/traintogain/backend/config/PersistenceConfig.java
+++ src/main/java/com/traintogain/backend/config/PersistenceConfig.java
@@ -5,12 +5,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.traintogain.backend.user")
-@EnableMongoRepositories(basePackages = {
-        "com.traintogain.backend.exercise",
-        "com.traintogain.backend.training",
-        "com.traintogain.backend.folder",
-        "com.traintogain.backend.auth.refreshtoken"
-})
+@EnableJpaRepositories(
+        basePackages = "com.traintogain.backend.user"
+)
+@EnableMongoRepositories(
+        basePackages = {
+                "com.traintogain.backend.exercise",
+                "com.traintogain.backend.training",
+                "com.traintogain.backend.folder",
+                "com.traintogain.backend.auth.refreshtoken",
+                "com.traintogain.backend.passwordreset"
+        }
+)
 public class PersistenceConfig {
 }
