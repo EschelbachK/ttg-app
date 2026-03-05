b99474c20248a49ded0afffdbc493d4ef8695995
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-03-05 16:22:50 +0100

    feat: add workout session repository

---
+++ src/main/java/com/traintogain/backend/workout/repository/WorkoutSessionRepository.java
@@ -0,0 +1,12 @@
+package com.traintogain.backend.workout.repository;
+
+import com.traintogain.backend.workout.model.WorkoutSession;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface WorkoutSessionRepository extends MongoRepository<WorkoutSession, String> {
+
+    List<WorkoutSession> findByUserId(String userId);
+
+}
