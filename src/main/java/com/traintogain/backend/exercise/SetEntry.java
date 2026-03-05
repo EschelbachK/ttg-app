e47835b4f74d776ab3a5373db839360242b1b3af
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-03-05 15:40:11 +0100

    fix in record

--- src/main/java/com/traintogain/backend/exercise/SetEntry.java
+++ src/main/java/com/traintogain/backend/exercise/SetEntry.java
@@ -1,20 +1,5 @@
 package com.traintogain.backend.exercise;
 
-public class SetEntry {
+public record SetEntry(double weight, int reps) {
 
-    private final double weight;
-    private final int reps;
-
-    public SetEntry(double weight, int reps) {
-        this.weight = weight;
-        this.reps = reps;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public int getReps() {
-        return reps;
-    }
 }
