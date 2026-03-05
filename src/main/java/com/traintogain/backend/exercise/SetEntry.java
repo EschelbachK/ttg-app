446cf07bb67c3fe40bfc4061c1c6bfea7bf82fd5
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-03-05 15:19:07 +0100

    eat: add SetEntry model for exercise sets

--- src/main/java/com/traintogain/backend/exercise/SetEntry.java
+++ src/main/java/com/traintogain/backend/exercise/SetEntry.java
@@ -2,50 +2,19 @@ package com.traintogain.backend.exercise;
 
 public class SetEntry {
 
-    private int order;        // Satz 1, 2, 3 …
-    private double weight;    // kg
-    private int repetitions; // Wiederholungen
-    private boolean completed;
+    private double weight;
+    private int reps;
 
-    public SetEntry() {
-    }
-
-    public SetEntry(int order, double weight, int repetitions) {
-        this.order = order;
+    public SetEntry(double weight, int reps) {
         this.weight = weight;
-        this.repetitions = repetitions;
-        this.completed = false;
-    }
-
-    public int getOrder() {
-        return order;
+        this.reps = reps;
     }
 
     public double getWeight() {
         return weight;
     }
 
-    public int getRepetitions() {
-        return repetitions;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setOrder(int order) {
-        this.order = order;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public void setRepetitions(int repetitions) {
-        this.repetitions = repetitions;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
+    public int getReps() {
+        return reps;
     }
 }
