22562f28b4ff7c2167355468c0db8def7d09c3ea
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-03-05 16:23:48 +0100

    feat: add workout session controller

---
+++ src/main/java/com/traintogain/backend/workout/controller/WorkoutSessionController.java
@@ -0,0 +1,32 @@
+package com.traintogain.backend.workout.controller;
+
+import com.traintogain.backend.workout.model.WorkoutSession;
+import com.traintogain.backend.workout.service.WorkoutSessionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/workouts")
+@RequiredArgsConstructor
+public class WorkoutSessionController {
+
+    private final WorkoutSessionService service;
+
+    @PostMapping("/start")
+    public WorkoutSession startWorkout(@RequestParam String userId) {
+        return service.startWorkout(userId);
+    }
+
+    @PostMapping("/finish")
+    public WorkoutSession finishWorkout(@RequestBody WorkoutSession session) {
+        return service.saveWorkout(session);
+    }
+
+    @GetMapping
+    public List<WorkoutSession> getWorkouts(@RequestParam String userId) {
+        return service.getUserWorkouts(userId);
+    }
+
+}
