cd33760660e385b798ee2e1318138a99bfa63c70
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-03-20 12:13:59 +0100

    feat: add NotFoundException for missing resources

--- src/main/java/com/traintogain/backend/exception/NotFoundException.java
+++ src/main/java/com/traintogain/backend/exception/NotFoundException.java
@@ -1,4 +1,8 @@
 package com.traintogain.backend.exception;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
