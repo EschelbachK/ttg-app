7c8bc0d2e42d0e6c53ad2710d048dccb1fb82a86
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-21 08:21:09 +0200

    simplify api response structure

--- src/main/java/com/traintogain/backend/api/ApiResponse.java
+++ src/main/java/com/traintogain/backend/api/ApiResponse.java
@@ -2,23 +2,18 @@ package com.traintogain.backend.api;
 
 public class ApiResponse<T> {
 
-    private boolean success;
-    private T data;
+    private final boolean success;
+    private final T data;
 
-    private ApiResponse(boolean success, T data) {
-        this.success = success;
+    private ApiResponse(T data) {
+        this.success = true;
         this.data = data;
     }
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(true, data);
+        return new ApiResponse<>(data);
     }
 
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public T getData() {
-        return data;
-    }
+    public boolean isSuccess() { return success; }
+    public T getData() { return data; }
 }
