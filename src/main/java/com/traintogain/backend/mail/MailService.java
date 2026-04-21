0232bf8142da8aedbba3ec6fd130597d65aded7c
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-21 08:24:19 +0200

    enhance mail service with sender configuration

--- src/main/java/com/traintogain/backend/mail/MailService.java
+++ src/main/java/com/traintogain/backend/mail/MailService.java
@@ -1,5 +1,6 @@
 package com.traintogain.backend.mail;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -7,18 +8,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class MailService {
 
-    private final JavaMailSender mailSender;
+    private final JavaMailSender sender;
 
-    public MailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public MailService(JavaMailSender sender) {
+        this.sender = sender;
     }
 
     public void send(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-
-        mailSender.send(message);
+        SimpleMailMessage m = new SimpleMailMessage();
+        m.setFrom(from);
+        m.setTo(to);
+        m.setSubject(subject);
+        m.setText(text);
+        sender.send(m);
     }
 }
