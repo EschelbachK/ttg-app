7ab1c74593115426e08b079d3a58d25373e232f2
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-23 12:06:35 +0200

    add verification token model

--- src/main/java/com/traintogain/backend/mail/verification/VerificationToken.java
+++ src/main/java/com/traintogain/backend/mail/verification/VerificationToken.java
@@ -1,4 +1,61 @@
 package com.traintogain.backend.mail.verification;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Document(collection = "verification_tokens")
 public class VerificationToken {
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    private String token;
+
+    private Instant expiryDate;
+
+    public VerificationToken() {
+    }
+
+    public VerificationToken(String userId, String token, Instant expiryDate) {
+        this.userId = userId;
+        this.token = token;
+        this.expiryDate = expiryDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public Instant getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setExpiryDate(Instant expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
 }
