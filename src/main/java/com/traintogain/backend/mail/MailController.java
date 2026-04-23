fda29126e3555f6d96d5eb09bafba6943d271959
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-23 12:05:48 +0200

    add mail controller

--- src/main/java/com/traintogain/backend/mail/MailController.java
+++ src/main/java/com/traintogain/backend/mail/MailController.java
@@ -1,4 +1,24 @@
 package com.traintogain.backend.mail;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MailController {
+
+    private final MailService mailService;
+
+    public MailController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @GetMapping("/mail-test")
+    public String sendTestMail() {
+        mailService.send(
+                "kai.eschelbach@gmx.net",
+                "TTG Test Mail",
+                "Email funktioniert"
+        );
+        return "mail sent";
+    }
 }
