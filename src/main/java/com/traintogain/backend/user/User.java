00ba259c024a9731373a200c557c50e7cd8d36fd
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-23 12:06:21 +0200

    add verification and reset fields to user

--- src/main/java/com/traintogain/backend/user/User.java
+++ src/main/java/com/traintogain/backend/user/User.java
@@ -1,8 +1,11 @@
 package com.traintogain.backend.user;
 
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
 
 @Document(collection = "users")
 public class User {
@@ -10,20 +13,46 @@ public class User {
     @Id
     private String id;
 
+    @Setter
     @Indexed(unique = true)
     private String email;
 
+    @Setter
+    @Indexed(unique = true)
     private String username;
+
+    @Setter
     private String password;
+
+    @Setter
     private Role role = Role.USER;
+
+    @Setter
+    private String resetToken;
+
+    @Setter
+    private Instant resetTokenExpiry;
+
+    @Setter
+    private boolean enabled = false;
+
+    @Setter
+    private String verificationToken;
+
+    @Setter
+    private Instant verificationTokenExpiry;
 
     public String getId() { return id; }
     public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
     public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
     public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
     public Role getRole() { return role; }
-    public void setRole(Role role) { this.role = role; }
+
+    public String getResetToken() { return resetToken; }
+    public Instant getResetTokenExpiry() { return resetTokenExpiry; }
+    public boolean isEnabled() { return enabled; }
+
+    public String getVerificationToken() { return verificationToken; }
+    public Instant getVerificationTokenExpiry() { return verificationTokenExpiry; }
+
 }
