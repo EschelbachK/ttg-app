c902292bf255c7a2d6d21bcefcac892d623398d9
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-24 08:26:32 +0200

    add mongodb indexes to exercise catalog

--- src/main/java/com/traintogain/backend/catalog/model/ExerciseCatalog.java
+++ src/main/java/com/traintogain/backend/catalog/model/ExerciseCatalog.java
@@ -2,6 +2,7 @@ package com.traintogain.backend.catalog.model;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -17,16 +18,20 @@ public class ExerciseCatalog {
     @Id
     private String id;
 
+    @Indexed
     private String name;
 
+    @Indexed
     private BodyRegion bodyRegion;
 
+    @Indexed
     private EquipmentType equipment;
 
     private String imageUrl;
 
     private String animationUrl;
 
+    @Indexed
     private Muscle primaryMuscle;
 
     private List<Muscle> secondaryMuscles;
@@ -35,6 +40,7 @@ public class ExerciseCatalog {
 
     private Difficulty difficulty;
 
+    @Indexed
     private MovementPattern movementPattern;
 
     private List<String> tags;
