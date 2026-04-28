b763bcf63f2420c584798a356917181af3720efe
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-28 19:19:44 +0200

    Add Progression model

--- src/main/java/com/traintogain/backend/catalog/model/Progression.java
+++ src/main/java/com/traintogain/backend/catalog/model/Progression.java
@@ -1,4 +1,15 @@
 package com.traintogain.backend.catalog.model;
 
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Progression {
+    private List<String> regressions;
+    private List<String> progressions;
 }
