2b796366286e75b93496caa9d817bddc092c5f78
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-28 19:19:47 +0200

    Add MediaAutoBuilder for media management

--- src/main/java/com/traintogain/backend/catalog/media/MediaAutoBuilder.java
+++ src/main/java/com/traintogain/backend/catalog/media/MediaAutoBuilder.java
@@ -1,4 +1,14 @@
 package com.traintogain.backend.catalog.media;
 
+import com.traintogain.backend.catalog.model.ExerciseMedia;
+
 public class MediaAutoBuilder {
+
+    public static ExerciseMedia build(String id) {
+        return ExerciseMedia.builder()
+                .image(id + ".jpg")
+                .thumbnail(id + "_thumb.jpg")
+                .animation(id + ".gif")
+                .build();
+    }
 }
