8c221ec249dbb279001fb3129f8f6a5463de2fba
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-28 19:19:41 +0200

    Add CreateTrainingExerciseRequest DTO

--- src/main/java/com/traintogain/backend/exercise/dto/CreateTrainingExerciseRequest.java
+++ src/main/java/com/traintogain/backend/exercise/dto/CreateTrainingExerciseRequest.java
@@ -1,19 +1,17 @@
 package com.traintogain.backend.exercise.dto;
 
-import com.traintogain.backend.common.BodyRegion;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.List;
 
+/**
+ * DTO für das Anlegen einer TrainingExercise.
+ */
 public record CreateTrainingExerciseRequest(
-        @NotBlank(message = "Übungsname darf nicht leer sein!")
-        String name,
 
         @NotBlank(message = "ExerciseId darf nicht leer sein!")
         String exerciseId,
-
-        BodyRegion bodyRegion,
 
         List<@Valid SetEntryRequest> sets
 ) {}
