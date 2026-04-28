8d80ac5e53ed9bde635f87970ca5d4e262fe25c9
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-28 19:19:43 +0200

    Add ExerciseMedia model

--- src/main/java/com/traintogain/backend/catalog/model/ExerciseMedia.java
+++ src/main/java/com/traintogain/backend/catalog/model/ExerciseMedia.java
@@ -1,4 +1,14 @@
 package com.traintogain.backend.catalog.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ExerciseMedia {
+    private String image;
+    private String thumbnail;
+    private String animation;
 }
