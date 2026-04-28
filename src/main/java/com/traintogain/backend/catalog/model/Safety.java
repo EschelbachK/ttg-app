a7c848b8567157e067ca43f77974f286df53163f
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-28 19:19:45 +0200

    Add Safety model

--- src/main/java/com/traintogain/backend/catalog/model/Safety.java
+++ src/main/java/com/traintogain/backend/catalog/model/Safety.java
@@ -1,4 +1,15 @@
 package com.traintogain.backend.catalog.model;
 
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Safety {
+    private RiskLevel riskLevel;
+    private List<String> contraindications;
 }
