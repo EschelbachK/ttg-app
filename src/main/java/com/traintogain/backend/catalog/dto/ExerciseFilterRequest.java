0f34782dbf5f53a8856bdf2dade1340cafd3be8d
EschelbachK <137324390+EschelbachK@users.noreply.github.com>
2026-04-28 19:19:43 +0200

    Add ExerciseFilterRequest DTO

--- src/main/java/com/traintogain/backend/catalog/dto/ExerciseFilterRequest.java
+++ src/main/java/com/traintogain/backend/catalog/dto/ExerciseFilterRequest.java
@@ -1,4 +1,19 @@
 package com.traintogain.backend.catalog.dto;
 
+import com.traintogain.backend.catalog.model.*;
+import lombok.Builder;
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+@Builder
 public class ExerciseFilterRequest {
+    private BodyRegion bodyRegion;
+    private Muscle muscle;
+    private EquipmentType equipment;
+    private MovementPattern pattern;
+    private List<ExerciseTag> tags;
+    private int page;
+    private int size;
+    private String sort;
 }
